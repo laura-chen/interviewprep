d6b919b562c73d95f785122c0d83cad2143eafe5
Laura Chen <lchen5@swarthmore.edu>
2017-11-29 14:44:53 -0500

    Given a dictionary whose values could be dictionaries, flatten it.

--- flattendictionary.py
+++ flattendictionary.py
@@ -1,16 +1,6 @@
-# Flatten a Dictionary
-#
-# A dictionary is a type of data structure that is supported natively in all
-# major interpreted languages such as JavaScript, Python, Ruby and PHP, where
-# it’s known as an Object, Dictionary, Hash and Array, respectively. In simple
-# terms, a dictionary is a collection of unique keys and their values. The values
-# can typically be of any primitive type (i.e an integer, boolean, double,
-# string etc) or other dictionaries (dictionaries can be nested). However, for
-# this exercise assume that values are either an integer, a string or another
-# dictionary.
-#
 # Given a dictionary dict, write a function flattenDictionary that returns a
-# flattened version of it .
+# flattened version of it . (Assume that values are either an integer, string,
+# or another dictionary.)
 
 # Example:
 # input:  dict = {
