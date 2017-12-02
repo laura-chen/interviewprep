d44344aebfda03b26587a3e6881eb03dacfb5606
Laura Chen <lchen5@swarthmore.edu>
2017-12-02 00:53:26 -0500

    Determine if two strings are one edit away.

--- oneaway.py
+++ oneaway.py
@@ -30,27 +30,3 @@ def one_away_pointer(s1, s2):
     if difference:
         return False
     return True
-
-# Hash table approach. O(n) time complexity and O(n) space complexity.
-def one_away(s1, s2):
-    if s1 == s2:
-        return True
-    length_diff = abs(len(s1)-len(s2))
-    if length_diff > 1:
-        return False
-    unique_chars = defaultdict(lambda:0)
-    for i in range(len(s1)):
-        unique_chars[s1[i]]+= 1
-    print unique_chars
-    for j in range(len(s2)):
-        if unique_chars[s2[j]] > 0:
-            unique_chars[s2[j]] -= 1
-    sum = 0
-    for key in unique_chars:
-        if unique_chars[key]:
-            sum += unique_chars[key]
-    print unique_chars
-    print sum
-    if sum > 1:
-        return False
-    return True
